d13a168e70e47cedf11ba25ffc578d6af433ce1f
2boom <97739181+2boom-ua@users.noreply.github.com>
2024-10-09 13:31:18 +0300

    Update ip_check.py

--- ip_check.py
+++ ip_check.py
@@ -9,19 +9,18 @@ import time
 import requests
 from schedule import every, repeat, run_pending
 
-
-def getExternalIp() -> str:
-	"""Function to get the external IP address from a list of URLs."""
+def getExternalIp():
 	for url in urls:
 		try:
 			response = requests.get(url)
-			return response.json().get('ip', "")
+			external_ip = response.json()
+			return external_ip['ip']
 		except requests.RequestException as e:
 			print(f"error {e}")
-	return ""
+	return None
 
 
-def getHostName() -> str:
+def getHostName():
 	"""Get system hostname"""
 	hostname = ""
 	hostname_path = '/proc/sys/kernel/hostname'
@@ -30,9 +29,9 @@ def getHostName() -> str:
 			hostname = file.read().strip()
 	return hostname
 
-
 def SendMessage(message: str):
-	"""Send notifications to various messaging services (Telegram, Discord, Slack, Gotify, Ntfy, Pushbullet, Pushover, Matrix, Mattermost, Rocket.chat)."""
+	"""Send notifications to various messaging services (Telegram, Discord, Gotify, Ntfy, Pushbullet, Pushover, Matrix, Zulip, Flock, Slack, RocketChat, Pumble, Mattermost, CUSTOM)."""
+	"""CUSTOM - single_asterisks - Zulip, Flock, Slack, RocketChat, Flock, double_asterisks - Pumble, Mattermost """
 	def SendRequest(url, json_data=None, data=None, headers=None):
 		"""Send an HTTP POST request and handle exceptions."""
 		try:
@@ -40,11 +39,38 @@ def SendMessage(message: str):
 			response.raise_for_status()
 		except requests.exceptions.RequestException as e:
 			print(f"Error sending message: {e}")
-	
+
 	if telegram_on:
 		for token, chat_id in zip(telegram_tokens, telegram_chat_ids):
 			url = f"https://api.telegram.org/bot{token}/sendMessage"
 			json_data = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
+			SendRequest(url, json_data)
+	if slack_on:
+		for url in slack_webhook_urls:
+			json_data = {"text": message}
+			SendRequest(url, json_data)
+	if rocket_on:
+		for url in rocket_webhook_urls:
+			json_data = {"text": message}
+			SendRequest(url, json_data)
+	if zulip_on:
+		for url in zulip_webhook_urls:
+			json_data = {"text": message}
+			SendRequest(url, json_data)
+	if flock_on:
+		for url in flock_webhook_urls:
+			json_data = {"text": message}
+			SendRequest(url, json_data)
+	if custom_on:
+		for url, std_bold in zip(custom_webhook_urls, custom_std_bolds):
+			bold_markdown = "**" if std_bold else "*"
+			json_data = {"text": message.replace("*", bold_markdown)}
+			SendRequest(url, json_data)
+	if matrix_on:
+		for token, server_url, room_id in zip(matrix_tokens, matrix_server_urls, matrix_room_ids):
+			url = f"{server_url}/_matrix/client/r0/rooms/{room_id}/send/m.room.message?access_token={token}"
+			matrix_message = "<br>".join(string.replace('*', '<b>', 1).replace('*', '</b>', 1) for string in message.split("\n"))
+			json_data = {"msgtype": "m.text", "body": matrix_message, "format": "org.matrix.custom.html", "formatted_body": matrix_message}
 			SendRequest(url, json_data)
 	if discord_on:
 		for url in discord_webhook_urls:
@@ -54,47 +80,35 @@ def SendMessage(message: str):
 		for url in mattermost_webhook_urls:
 			json_data = {'text': message.replace("*", "**")}
 			SendRequest(url, json_data)
-	if slack_on:
-		for url in slack_webhook_urls:
-			json_data = {"text": message}
-			SendRequest(url, json_data)
-	if matrix_on:
-		for token, server_url, room_id in zip(matrix_tokens, matrix_server_urls, matrix_room_ids):
-			url = f"{server_url}/_matrix/client/r0/rooms/{room_id}/send/m.room.message?access_token={token}"
-			tmp_message = "<br>".join(string.replace('*', '<b>', 1).replace('*', '</b>', 1) for string in message.split("\n"))
-			json_data = {"msgtype": "m.text", "body": tmp_message, "format": "org.matrix.custom.html", "formatted_body": tmp_message}
-			SendRequest(url, json_data)
-	if rocket_on:
-		for token, server_url, user_id, channel in zip(rocket_tokens, rocket_server_urls,rocket_user_ids, rocket_channel_ids):
-			url = f"{server_url}/api/v1/chat.postMessage"
-			headers_data = {"X-Auth-Token": token, "X-User-Id": user_id, "Content-Type": "application/json"}
-			json_data = {"channel": channel, "text": message}
-			SendRequest(url, json_data, None, headers_data)
-	
-	header, message = message.replace("*", "").split("\n", 1)
-	message = message.strip()
-	
-	if gotify_on:
-		for token, server_url in zip(gotify_tokens, gotify_server_urls):
-			url = f"{server_url}/message?token={token}"
-			json_data = {'title': header, 'message': message, 'priority': 0}
+	if pumble_on:
+		for url in pumble_webhook_urls:
+			json_data = {"text": message.replace("*", "**")}
 			SendRequest(url, json_data)
 	if ntfy_on:
 		for url in ntfy_webhook_urls:
-			encoded_message = message.encode(encoding = 'utf-8')
-			headers_data = {"title": header}
-			SendRequest(url, None, encoded_message, headers_data)
-	if pushbullet_on:
-		for token in pushbullet_tokens:
-			url = "https://api.pushbullet.com/v2/pushes"
-			json_data = {'type': 'note', 'title': header, 'body': message}
-			headers_data = {'Access-Token': token, 'Content-Type': 'application/json'}
-			SendRequest(url, json_data, None, headers_data)
+			headers_data = {"Markdown": "yes"}
+			SendRequest(url, None, message.replace("*", "**").encode(encoding = "utf-8"), headers_data)
+
+	header, message = message.split("\n", 1)
+	message = message.strip()
+
+	if gotify_on:
+		for token, server_url in zip(gotify_tokens, gotify_server_urls):
+			url = f"{server_url}/message?token={token}"
+			json_data = {'title': header.replace("*", ""), "message": message.replace("*", "**").replace("\n", "\n\n"), "priority": 0, "extras": {"client::display": {"contentType": "text/markdown"}}}
+			SendRequest(url, json_data)
 	if pushover_on:
 		for token, user_key in zip(pushover_tokens, pushover_user_keys):
 			url = "https://api.pushover.net/1/messages.json"
-			json_data = {"token": token, "user": user_key, "message": message, "title": header}
+			pushover_message = "\n".join(string.replace('*', '<b>', 1).replace('*', '</b>', 1) for string in message.split("\n"))
+			json_data = {"token": token, "user": user_key, "message": pushover_message, "title": header.replace("*", ""), "html": "1"}
 			SendRequest(url, json_data)
+	if pushbullet_on:
+		for token in pushbullet_tokens:
+			url = "https://api.pushbullet.com/v2/pushes"
+			json_data = {'type': 'note', 'title': header.replace("*", ""), 'body': message.replace("*", "")}
+			headers_data = {'Access-Token': token, 'Content-Type': 'application/json'}
+			SendRequest(url, json_data, None, headers_data)
 
 
 if __name__ == "__main__":
@@ -111,14 +125,14 @@ if __name__ == "__main__":
 		default_dot_style = parsed_json["DEFAULT_DOT_STYLE"]
 		urls = parsed_json["SERVICE_URLS"]
 		header_message = f"*{host_name}* (ip.check)\n"	
-		current_ip = getExternalIp()
-		old_ip_address = current_ip
-		monitoring_mg = f"- current ip: *{current_ip}*\n"
+		external_ip = getExternalIp()
+		old_ip_address = external_ip
+		monitoring_mg = f"- current ip: *{external_ip}*\n"
 		if not default_dot_style:
 			dots = square_dots
 		orange_dot, green_dot, red_dot, yellow_dot = dots["orange"], dots["green"], dots["red"], dots["yellow"]
-		messaging_platforms = ["TELEGRAM", "DISCORD", "GOTIFY", "NTFY", "PUSHBULLET", "PUSHOVER", "SLACK", "MATRIX", "MATTERMOST", "ROCKET"]
-		telegram_on, discord_on, gotify_on, ntfy_on, pushbullet_on, pushover_on, slack_on, matrix_on, mattermost_on, rocket_on = (parsed_json[key]["ON"] for key in messaging_platforms)
+		messaging_platforms = ["TELEGRAM", "DISCORD", "GOTIFY", "NTFY", "PUSHBULLET", "PUSHOVER", "SLACK", "MATRIX", "MATTERMOST", "PUMBLE", "ROCKET", "ZULIP", "FLOCK", "CUSTOM"]
+		telegram_on, discord_on, gotify_on, ntfy_on, pushbullet_on, pushover_on, slack_on, matrix_on, mattermost_on, pumble_on, rocket_on, zulip_on, flock_on, custom_on = (parsed_json[key]["ON"] for key in messaging_platforms)
 		services = {
 			"TELEGRAM": ["TOKENS", "CHAT_IDS"],
 			"DISCORD": ["WEBHOOK_URLS"],
@@ -129,7 +143,11 @@ if __name__ == "__main__":
 			"PUSHOVER": ["TOKENS", "USER_KEYS"],
 			"MATRIX": ["TOKENS", "SERVER_URLS", "ROOM_IDS"],
 			"MATTERMOST": ["WEBHOOK_URLS"],
-			"ROCKET": ["TOKENS", "SERVER_URLS", "USER_IDS", "CHANNEL_IDS"]
+			"PUMBLE": ["WEBHOOK_URLS"],
+			"ROCKET": ["WEBHOOK_URLS"],
+			"ZULIP": ["WEBHOOK_URLS"],
+			"FLOCK": ["WEBHOOK_URLS"],
+			"CUSTOM": ["WEBHOOK_URLS", "STD_BOLDS"]
 		}
 		for service, keys in services.items():
 			if parsed_json[service]["ON"]:
@@ -143,12 +161,11 @@ if __name__ == "__main__":
 
 @repeat(every(min_repeat).minutes)
 def CheckIP():
-	"""Function to check the IP periodically."""
 	global old_ip_address
-	current_ip = getExternalIp()
-	if old_ip_address != current_ip:
-		old_ip_address = current_ip
-		monitoring_mg = f"{orange_dot} new ip is: *{current_ip}*\n"
+	external_ip = getExternalIp()
+	if old_ip_address != external_ip: # !=
+		old_ip_address = external_ip
+		monitoring_mg = f"{orange_dot} new ip is: *{str(external_ip)}*\n"
 		SendMessage(f"{header_message}{monitoring_mg}")
 
 while True:
