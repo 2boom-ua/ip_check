fce8180ae52fa35e0e0ddbee184aa6261828a50b
2boom <97739181+2boom-ua@users.noreply.github.com>
2024-11-06 21:01:12 +0200

    Add files via upload

--- ip_check.py
+++ ip_check.py
@@ -183,56 +183,46 @@ if __name__ == "__main__":
 		local_ip = getLocalIp()
 		old_ip_address = external_ip
 		old_ip_local = local_ip
-		monitoring_mg = f"- public ip: *{external_ip}*\n- local  ip: *{local_ip}*\n"
+		monitoring_message = f"- public ip: *{external_ip}*\n- local  ip: *{local_ip}*\n"
 		if not default_dot_style:
 			dots = square_dots
 		orange_dot, green_dot, red_dot, yellow_dot = dots["orange"], dots["green"], dots["red"], dots["yellow"]
-		messaging_platforms = ["TELEGRAM", "DISCORD", "GOTIFY", "NTFY", "PUSHBULLET", "PUSHOVER", "SLACK", "MATRIX", "MATTERMOST", "PUMBLE", "ROCKET", "ZULIP", "FLOCK", "APPRISE", "CUSTOM"]
-		telegram_on, discord_on, gotify_on, ntfy_on, pushbullet_on, pushover_on, slack_on, matrix_on, mattermost_on, pumble_on, rocket_on, zulip_on, flock_on, apprise_on, custom_on = (config_json[key]["ENABLED"] for key in messaging_platforms)
-		services = {
-			"TELEGRAM": ["TOKENS", "CHAT_IDS"],
-			"DISCORD": ["WEBHOOK_URLS"],
-			"SLACK": ["WEBHOOK_URLS"],
-			"GOTIFY": ["TOKENS", "SERVER_URLS"],
-			"NTFY": ["WEBHOOK_URLS"],
-			"PUSHBULLET": ["TOKENS"],
-			"PUSHOVER": ["TOKENS", "USER_KEYS"],
-			"MATRIX": ["TOKENS", "SERVER_URLS", "ROOM_IDS"],
-			"MATTERMOST": ["WEBHOOK_URLS"],
-			"PUMBLE": ["WEBHOOK_URLS"],
-			"ROCKET": ["WEBHOOK_URLS"],
-			"ZULIP": ["WEBHOOK_URLS"],
-			"FLOCK": ["WEBHOOK_URLS"],
-			"APPRISE": ["WEBHOOK_URLS", "FORMAT_MESSAGES"],
-			"CUSTOM": ["WEBHOOK_URLS", "HEADERS", "PYLOADS", "FORMAT_MESSAGES"]
-		}
-		for service, keys in services.items():
-			if config_json[service]["ENABLED"]:
-				globals().update({f"{service.lower()}_{key.lower()}": config_json[service][key] for key in keys})
-				monitoring_mg += f"- messaging: {service.capitalize()},\n"
-		monitoring_mg += (
+		no_messaging_keys = ["DEFAULT_DOT_STYLE", "MIN_REPEAT", "SERVICE_URLS"]
+		messaging_platforms = list(set(config_json) - set(no_messaging_keys))
+		for platform in [
+			"telegram_on", "discord_on", "gotify_on", "ntfy_on", "pushbullet_on", 
+			"pushover_on", "slack_on", "matrix_on", "mattermost_on", "pumble_on", 
+			"rocket_on", "zulip_on", "flock_on", "apprise_on", "custom_on"]:
+			globals()[platform] = False
+		globals().update({f"{key.lower()}_on": config_json[key]["ENABLED"] for key in messaging_platforms})
+		for platform in messaging_platforms:
+			if config_json[platform].get("ENABLED", False):
+				for key, value in config_json[platform].items():
+					globals()[f"{platform.lower()}_{key.lower()}"] = value
+				monitoring_message += f"- messaging: {platform.lower().capitalize()},\n"
+		monitoring_message += (
 			f"- default dot style: {default_dot_style}.\n"
 			f"- polling period: {min_repeat} minute(s)."
 		)
-		SendMessage(f"{header_message}hosts monitor:\n{monitoring_mg}")
+		SendMessage(f"{header_message}hosts monitor:\n{monitoring_message}")
 	else:
 		print("config.json not found")
 		
 
 @repeat(every(min_repeat).minutes)
 def CheckIP():
-	monitoring_mg = ""
+	monitoring_message = ""
 	global old_ip_address, old_ip_local
 	external_ip = getExternalIp()
 	local_ip = getLocalIp()
 	if old_ip_address != external_ip: # !=
 		old_ip_address = external_ip
-		monitoring_mg += f"{orange_dot} new public ip: *{str(external_ip)}*\n"
+		monitoring_message += f"{orange_dot} new public ip: *{str(external_ip)}*\n"
 	if old_ip_local != local_ip:
 		old_ip_local = local_ip
-		monitoring_mg += f"{orange_dot} new local ip: *{str(external_ip)}*\n"
-	if monitoring_mg:
-		SendMessage(f"{header_message}{monitoring_mg}")
+		monitoring_message += f"{orange_dot} new local ip: *{str(external_ip)}*\n"
+	if monitoring_message:
+		SendMessage(f"{header_message}{monitoring_message}")
 
 while True:
 	run_pending()
